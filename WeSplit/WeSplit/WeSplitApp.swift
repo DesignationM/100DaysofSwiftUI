6efaab0c49414deb17adedffcb1d0d1379b25d2b
DesignationM <jmdye@live.com>
2023-05-27 23:16:02 -0400

    Project 1 - WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by KogaWolfe on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
