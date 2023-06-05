b73c9ed0cd20501c1e14ca9f3348360a6b8c6a84
DesignationM <jmdye@live.com>
2023-06-04 20:59:30 -0400

    Day 25 challenge

---
+++ RockPaperScissor/RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by KogaWolfe on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
