2c258325b512674a91ba67317c439be011ac06d6
DesignationM <jmdye@live.com>
2023-06-12 22:09:58 -0400

    Animations 1 & 2

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,9 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var animationAmount = 0.0
-
+	@State private var dragAmount = CGSize.zero
+	@State private var enabled = false
+	let letters = Array("Hello SwiftUI")
     var body: some View {
 		VStack {
+			HStack(spacing: 0) {
+				ForEach(0..<letters.count, id: \.self) { num in
+					Text(String(letters[num]))
+						.padding(5)
+						.font(.title)
+						.background(enabled ? .blue : .red)
+						.offset(dragAmount)
+						.animation(.default.delay(Double(num) / 20), value: dragAmount)
+				}
+			}
+			.gesture(
+			DragGesture()
+				.onChanged { dragAmount = $0.translation}
+				.onEnded { _ in
+					dragAmount = .zero
+					enabled.toggle()
+				})
+			Spacer()
+			LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+				.frame(width: 300, height: 200)
+				.clipShape(RoundedRectangle(cornerRadius: 10))
+				.offset(dragAmount)
+				.gesture(
+					DragGesture()
+					.onChanged { dragAmount = $0.translation }
+						.onEnded { _ in //dragAmount = .zero}
+							withAnimation(.spring()) {
+								dragAmount = .zero
+							}
+						}
+				)
+				
 		//	Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
 			
 		//	Spacer()
