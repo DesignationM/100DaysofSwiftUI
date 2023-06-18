94dd4d7fee91beb0f51fe8f1264ac5f0b57fe301
DesignationM <jmdye@live.com>
2023-06-17 20:23:35 -0400

    IExpense Wrap Up

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -1,7 +1,7 @@
 //
 //  ContentView.swift
 //  iExpense
-//TextField("First Name", text: $user.firstName)//  Created by KogaWolfe on 6/15/23.
+//  Created by KogaWolfe on 6/15/23.
 //
 
 import SwiftUI
@@ -9,22 +9,63 @@ import SwiftUI
 struct ContentView: View {
 	@StateObject var expenses = Expenses()
 	@State private var showingAddExpense = false
-	
+
     var body: some View {
 		NavigationView {
 			List  {
-				ForEach(expenses.items) {item in
-					HStack {
-						VStack(alignment: .leading) {
-							Text(item.name)
-								.font(.headline)
-							Text(item.type)
+				/// Challenge 3: For a bigger challenge, try splitting the expenses list into two section:
+				///  one for personal expenses and one for business expenses.
+				Section {
+					ForEach(expenses.items) {item in
+						if item.type == "Personal" {
+							HStack {
+								VStack(alignment: .leading) {
+									Text(item.name)
+										.font(.headline)
+									
+									Text(item.type)
+								}
+								Spacer()
+				/// Challenge 1: Use the users preferred currency, rather than always using US Dollars
+				/// Challenge 2: Modify the expense amounts in ContentView to contain some styling
+				/// depending on their value - expenses under $10 should have one style, expenses under
+				/// $100 another and expenses over $100 a third. What those styles are depend on you
+								Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+									.foregroundColor(textColor(amount: item.amount))
+								//						} else if item.amount > 10 && item.amount < 100 {
+								//							.foregroundColor(.red)
+								//						}
+								
+							}
 						}
-						Spacer()
-						Text(item.amount, format: .currency(code: "USD"))
-					}
+					}.onDelete(perform: removeItems)
+				} header: {
+					Text("Personal expenses")
 				}
-				.onDelete(perform: removeItems)
+				Section {
+					ForEach(expenses.items) { seconditem in
+						if seconditem.type == "Business" {
+							HStack {
+								VStack(alignment: .leading) {
+									Text(seconditem.name)
+										.font(.headline)
+									
+									Text(seconditem.type)
+								}
+								Spacer()
+				/// Challenge 1: Use the users preferred currency, rather than always using US Dollars
+				/// Challenge 2: Modify the expense amounts in ContentView to contain some styling
+				/// depending on their value - expenses under $10 should have one style, expenses under
+				/// $100 another and expenses over $100 a third. What those styles are depend on you
+								Text(seconditem.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+									.foregroundColor(textColor(amount: seconditem.amount))
+							}
+						}
+					}.onDelete(perform: removeItems)
+				} header: {
+					Text("Business expenses")
+				}
+				
 			}
 			.navigationTitle("iExpense")
 			.toolbar {
@@ -39,6 +80,16 @@ struct ContentView: View {
 			}
 		}
 	
+	}
+	
+	func textColor(amount: Double) -> Color {
+		if amount > 100 {
+			return .green
+		} else if amount > 10 {
+			return .gray
+		} else {
+			return .red
+		}
 	}
 	
 	func removeItems(at offsets: IndexSet) {
