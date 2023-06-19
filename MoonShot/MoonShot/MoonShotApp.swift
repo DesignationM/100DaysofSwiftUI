f4c5158714ef87b29995a6074399f5572383c8a2
DesignationM <jmdye@live.com>
2023-06-18 22:42:48 -0400

    Day 39

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by KogaWolfe on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
