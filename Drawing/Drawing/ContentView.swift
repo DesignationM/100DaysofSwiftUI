8e35df6de97b9489e4363587cce08e1c7c781d8a
DesignationM <jmdye@live.com>
2023-06-23 20:17:44 -0400

    Drawing Group Day 44

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,24 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var petalOffset = -20.0
+	@State private var petalWidth = 100.0
+	
+	@State private var colorCycle = 0.0
     var body: some View {
-        VStack {
-			Triangle()
-			// .fill(.blue)
-			.stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-			
-			Triangle()
-				.fill(.red)
-				.frame(width: 300, height: 300)
-			
-			Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockWise: true)
-				.stroke(.blue, lineWidth: 10)
-				.frame(width: 300, height: 300)
-			
-			Circle()
-				.strokeBorder(.blue, lineWidth: 40)
-        }
-        .padding()
+		ZStack {
+			VStack {
+				ColorCyclingCircle(amount: colorCycle)
+					.frame(width: 300, height: 300)
+				
+				Slider(value: $colorCycle)
+	//			Triangle()
+	//			// .fill(.blue)
+	//			.stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+	//
+	//			Triangle()
+	//				.fill(.red)
+	//				.frame(width: 300, height: 300)
+	//
+	//			Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockWise: true)
+	//				.stroke(.blue, lineWidth: 10)
+	//				.frame(width: 300, height: 300)
+	//
+	//			Circle()
+	//				.strokeBorder(.blue, lineWidth: 40)
+				Flower(petalOffSet: petalOffset, petalWidth: petalWidth)
+					.fill(.red, style: FillStyle(eoFill: true))
+					//.stroke(.red, lineWidth: 1)
+				
+				Text("Offset")
+				Slider(value: $petalOffset, in: -40...40)
+					.padding([.horizontal, .bottom])
+				
+				Text("Width")
+				Slider(value: $petalWidth, in: 0...100)
+					.padding(.horizontal)
+			}
+			.padding()
+		}
+		.drawingGroup()
     }
 }
 
@@ -61,6 +83,65 @@ struct Arc: InsettableShape {
 		var arc = self
 		arc.insetAmount += amount
 		return arc
+	}
+}
+
+struct Flower: Shape {
+	// How much to move this petal away from the center
+	var petalOffSet: Double = -20
+	
+	// How wide to make each petal
+	var petalWidth: Double = 100
+	
+	func path(in rect: CGRect) -> Path {
+		// The path that will hold all petals
+		var path = Path()
+		
+		// Count from 0 up to pi * 2, moving up pi / 8 each time
+		for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+			// rotate the petal by the current value of our loop
+			let rotation = CGAffineTransform(rotationAngle: number)
+			
+			// move the petal to be at the center of our view
+			let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+			
+			// create a path for this petal using our properties plus a fixed Y and height
+			let originalPetal = Path(ellipseIn: CGRect(x: petalOffSet, y: 0, width: petalWidth, height: rect.width / 2))
+			
+			// apply our rotatin/position transformation to the petal
+			let rotatedPetal = originalPetal.applying(position)
+			
+			// add it to our main path
+			path.addPath(rotatedPetal)
+		}
+		
+		// now send the main path back
+		return path
+	}
+}
+
+struct ColorCyclingCircle: View {
+	var amount = 0.0
+	var steps = 100
+	
+	var body: some View {
+		ZStack {
+			ForEach(0..<steps) { value in
+				Circle()
+					.inset(by: Double(value))
+					.strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+			}
+		}
+	}
+	
+	func color(for value: Int, brightness: Double) -> Color {
+		var targetHue = Double(value) / Double(steps) + amount
+		
+		if targetHue > 1 {
+			targetHue -= 1
+		}
+		
+		return Color(hue: targetHue, saturation: 1, brightness: brightness)
 	}
 }
 
