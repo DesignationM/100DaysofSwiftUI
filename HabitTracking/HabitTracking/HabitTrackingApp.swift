1c161bfc9f4dc213d7f3a8e07de24d12c7860903
DesignationM <jmdye@live.com>
2023-06-27 23:00:24 -0400

    Day 47
    Milestone: projects 7-9

---
+++ HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by KogaWolfe on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
