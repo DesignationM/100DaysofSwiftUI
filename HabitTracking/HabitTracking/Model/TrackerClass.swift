5fa341a5f6d0825fe2a4561e481728e0a0f28ec2
DesignationM <jmdye@live.com>
2023-06-28 00:05:55 -0400

    Usable challenge
    Use codable and UserDefaults to load and save all your data.

--- HabitTracking/HabitTracking/Model/TrackerClass.swift
+++ HabitTracking/HabitTracking/Model/TrackerClass.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Tracker: ObservableObject {
-	@Published var trackedItems = [Item]()
+	@Published var trackedItems = [Item]() {
+		didSet {
+			if let encoded = try? JSONEncoder().encode(trackedItems) {
+				UserDefaults.standard.set(encoded, forKey: "Items")
+			}
+		}
+	}
+	
+	init() {
+		if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+			if let decodedItems = try? JSONDecoder().decode([Item].self, from: savedItems) {
+				trackedItems = decodedItems
+				return
+			}
+		}
+		
+		trackedItems = []
+	}
 }
